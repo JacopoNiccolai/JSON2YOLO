70441352da547dd66c9cefa42bd317e083bcdbeb
JacopoNiccolai <jacoponiccolai99@gmail.com>
2025-04-28 22:19:54 +0200

    Update convert_coco_json call to include use_segments and cls91to80 parameters

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
 from general_json2yolo import convert_coco_json
 
-convert_coco_json(json_dir="data/input", labels_dir="data/output")
+convert_coco_json(json_dir="data/input", labels_dir="data/output", use_segments=False, cls91to80=True)
 	
